dbb698ea6ff50dd16c8e6360ea948f0770dbf20d
Kendall <ktrudickm@gmail.com>
2021-05-27 18:04:50 -0400

    FInished checkDatabase function and saveRecord

--- public/database.js
+++ public/database.js
@@ -12,21 +12,21 @@ let db;
 
 req.onupgradeneeded = ({target}) => {
      db = target.result;
-
      db.createObjectStore('pending');
 };
 
 req.onsuccess = ({target}) => {
-    db=target.result;
-
+    db = target.result;
     if(navigator.onLine){
         checkDatabase();
     };
-    
 }
 
-
-
+function saveRecord(transaction) {
+    const transact = db.transaction('pending', 'readwrite');
+    const store = transact.objectStore('pending');
+    store.add(transaction);
+}
 
 function checkDatabase(){
     const transact = db.transaction(["pending"]);
@@ -34,13 +34,24 @@ function checkDatabase(){
     const getAll =store.getAll();
 
 
-    getAll.onsuccess =function(){
+    getAll.onsuccess = function(){
         if (getAll.result.length > 0){
-            console.log('FETCH HERE')
+         fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+          .then((resp) => resp.json())
+          .then(() => {
+              const transact = db.transaction("pending", "readwrite");
+              const store = transact.objectStore("pending");
+              store.clear();
+          })
         }
     }
-
-
 }
 
 window.addEventListener("online", checkDatabase);
