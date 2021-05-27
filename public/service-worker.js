7e2f69a9fa2d52b7ab75c636f9b01ad13d0ec9df
Kendall <ktrudickm@gmail.com>
2021-05-27 18:32:26 -0400

    Still cannot get cache addAll to work. Making adjustments to try and resolve

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,22 @@
 const FILES_TO_CACHE = [ 
-    '/', 
+    '/',
     '/index.html',
     '/index.js',
     '/style.css',
     '/database.js',
-    './icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
   ];
 
   const CACHE_NAME = "static-cache-v1";
   const DATA_CACHE_NAME = "data-cache-v1";
 
-  self.addEventListener('install', (event) => {
-      event.waitUntil(
-          caches
-            .open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
-          );
-        self.skipWaiting()
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
   });
 
 
