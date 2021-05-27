2b98dcc61c15c1ff17674c51499c59d6c1b3d15c
Kendall <ktrudickm@gmail.com>
2021-05-27 12:43:14 -0400

    ran npm install. Completed service-worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,4 +13,50 @@ const FILES_TO_CACHE = [
   const CACHE_NAME = "static-cache-v1";
   const DATA_CACHE_NAME = "data-cache-v1";
 
-  
+  self.addEventListener('install', (event) => {
+      event.waitUntil(
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+          );
+  });
+
+  self.addEventListener('activate', (event) => {
+      const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+      event.waitUntil(
+          caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+      );
+  });
+
+  self.addEventListener('fetch', (event) => {
+      if (event.request.url.startsWith(self.location.origin)) {
+          event.respondWith(
+              caches.match(event.request).then((cachedResponse) => {
+                  if (cachedResponse) {
+                      return cachedResponse;
+                  }
+
+                  return caches.open(RUNTIME).then((cache) => {
+                      return fetch(event.request).then((response) => {
+                          return cache.put(event.request, response.clone()).then(() => {
+                              return response;
+                          })
+                      })
+                  })
+              })
+          )
+      }
+  })
